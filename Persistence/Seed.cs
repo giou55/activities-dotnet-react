17617f56680c9cca6ed9d13221eb51b316e8b037
giou55 <giourmetakis00@gmail.com>
2023-05-01 14:34:36 +0300

    changes

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -41,12 +41,16 @@ namespace Persistence
                 {
                     new Activity
                     {
-                        Title = "Past Activity 1",
+                        Title = "Πεζοπορία νυχτερινή με πανσέληνο στο ιστορικό κέντρο της Αθήνας",
                         Date = DateTime.UtcNow.AddMonths(-2),
-                        Description = "Activity 2 months ago",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Pub",
+                        Description = @"Νυχτερινη πεζοπορία με πανσέληνο, στις 5 Μαιου 2023 Παρασκευη, στο ιστορικό κέντρο της Αθήνας περιμετρικά της Ακρόπολης,
+                                    με ηλιοβασίλεμα στον Άρειο Πάγο, με πανσεληνο στο λόφο της ΠΝΥΚΑΣ. Στη συνέχεια προαιρετικα σε ταβέρνα στο μοναστηρακι..
+                                    Σημείο συνάντησης σταθμός Ηλεκτρικού Θησείο και ώρα 18.54 μ.μ. Η συμμετοχή είναι δωρεάν οι συμμετέχοντες να είναι υγιείς σε καλή φυσική κατάσταση,
+                                    παπούτσια κατάλληλα για χώμα, άσφαλτο, βράχια, σακίδιο νερό, καπέλο, αντιανεμικο, ξηρά τροφή.
+                                    Η διαδρομή είναι περίπου 3.00 έως 3.30 ώρες.",
+                        Category = "πεζοπορία",
+                        City = "Αθήνα",
+                        Venue = "Σταθμός μετρό Θησείου",
                         Attendees = new List<ActivityAttendee>
                         {
                             new ActivityAttendee
@@ -58,12 +62,17 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Past Activity 2",
+                        Title = "Σεμινάριο συλλογής και διατήρησης σπόρων",
                         Date = DateTime.UtcNow.AddMonths(-1),
-                        Description = "Activity 1 month ago",
-                        Category = "culture",
-                        City = "Paris",
-                        Venue = "The Louvre",
+                        Description = @"Μέσα από παρουσιάσεις και συζητήσεις, αυτό το σεμινάριο, έχει στόχο να παράσχει μια πλήρη κατανόηση της
+                            αξίας των παραδοσιακών ποικιλιών σπόρων καθώς και την απαραίτητη τεχνική εκπαίδευση στη συλλογή και τη διατήρηση τους.
+                            Πραγματοποιείται διαδικτυακά (συνολική διαρκεια 8 ώρες).
+                            Όσοι/ες το παρακολουθήσουν, μετά το τέλος του σεμιναρίου θα λάβουν δωρεάν ταχυδρομικά 3 παραδοσιακές ποικιλίες λαχανικών
+                            από την τράπεζα σπόρων της Νέας Γουινέας για να εξασκήσουν τις γνώσεις που απέκτησαν! Επίσης θα λάβουν τις παρουσιάσεις
+                            του σεμιναρίου και πολύ επιπλέον υλικό για διάβασμα. Κόστος συμμετοχής: 40 ευρώ.",
+                        Category = "σεμινάριο",
+                        City = "Everywhere",
+                        Venue = "Online",
                         Attendees = new List<ActivityAttendee>
                         {
                             new ActivityAttendee
@@ -80,12 +89,13 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 1",
+                        Title = "Φαντάσματα Εφηβείας (Ghost World) // Τέρι Ζουίγκοφ",
                         Date = DateTime.UtcNow.AddMonths(1),
-                        Description = "Activity 1 month in future",
-                        Category = "music",
-                        City = "London",
-                        Venue = "Wembly Stadium",
+                        Description = @"Αυτή την εβδομάδα στο Cinemarian, μία όχι και τόσο συνηθισμένη teen movie, βασισμένη στο ομότιτλο κόμικ του Ντάνιελ Κλόουζ.
+                            Δεν κάνουμε κρατήσεις, τα εισιτήρια μπορείτε να τα προμηθευτείτε από το ταμείο μας πριν την προβολή. Τηρείται η σειρά προτεραιότητας.",
+                        Category = "φιλμ",
+                        City = "Αθήνα",
+                        Venue = "Cinemarian, Σισμάνη 16",
                         Attendees = new List<ActivityAttendee>
                         {
                             new ActivityAttendee
@@ -102,12 +112,21 @@ namespace Persistence
                     },
                     new Activity
                     {
-                        Title = "Future Activity 2",
+                        Title = "ΕΥΡΥΔΙΚΗ – ΜΠΛΕ Ζήσε τη στιγμή!",
                         Date = DateTime.UtcNow.AddMonths(2),
-                        Description = "Activity 2 months in future",
-                        Category = "food",
-                        City = "London",
-                        Venue = "Jamies Italian",
+                        Description = @"Η Ευρυδίκη και οι Μπλε μας προτρέπουν: Ζήστε μαζί μας τη στιγμή!
+                            Ύστερα από αλλεπάλληλες sοld out εμφανίσεις σε Αθήνα, Θεσσαλονίκη και Κύπρο, η Ευρυδίκη και οι Μπλε έρχονται για να βάλουν φωτιά στη σκηνή του CT Garden.
+                            Οι δύο πιο ξεχωριστές ποπ-ροκ φωνές της ελληνικής μουσικής, η Ευρυδίκη και η Τζώρτζια αποτελούν ένα εκρηκτικό μείγμα γεμάτο εξωστρέφεια και φρεσκάδα.
+                            Η καταξιωμένη Ευρυδίκη, με μακρόχρονη πορεία στο τραγούδι, που διακρίνεται για το ηχόχρωμα της φωνής της και τις ανατρεπτικές συνεργασίες,
+                            μαζί με το ξεχωριστό συγκρότημα των Μπλε,
+                            με την ιδιαίτερη αισθητική στη μουσική και στον στίχο και την πολυεπίπεδη “front woman” Τζώρτζια Κεφαλά, συνεργάζονται συναυλιακά και δισκογραφικά.
+                            “Άκου την Ζωή” είναι ο τίτλος του ντουέτου τους που κυκλοφόρησε πρόσφατα με τις φωνές της Ευρυδίκης και της Τζώρτζιας, σε μουσική Γιώργου Παπαποστόλου,
+                            συνθέτη και παραγωγού των Μπλε και στίχους του Γιώργου Παρώδη.
+                            Η Ευρυδίκη και οι Μπλε σε αυτήν τη μουσική συνάντηση στο CT Garden θα μας παρουσιάσουν τα αγαπημένα τους τραγούδια και μια σειρά από ντουέτα “έκπληξη”,
+                            ελληνικά και ξένα, χωρίς να λείπουν φυσικά όλες οι μεγάλες τους επιτυχίες.",
+                        Category = "μουσική",
+                        City = "Αθήνα",
+                        Venue = "Christmas Theater",
                         Attendees = new List<ActivityAttendee>
                         {
                             new ActivityAttendee
@@ -119,133 +138,6 @@ namespace Persistence
                             {
                                 AppUser = users[2],
                                 IsHost = false
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 3",
-                        Date = DateTime.UtcNow.AddMonths(3),
-                        Description = "Activity 3 months in future",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Pub",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = true                            
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[0],
-                                IsHost = false                            
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 4",
-                        Date = DateTime.UtcNow.AddMonths(4),
-                        Description = "Activity 4 months in future",
-                        Category = "culture",
-                        City = "London",
-                        Venue = "British Museum",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = true                            
-                            }
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 5",
-                        Date = DateTime.UtcNow.AddMonths(5),
-                        Description = "Activity 5 months in future",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Punch and Judy",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[0],
-                                IsHost = true                            
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = false                            
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 6",
-                        Date = DateTime.UtcNow.AddMonths(6),
-                        Description = "Activity 6 months in future",
-                        Category = "music",
-                        City = "London",
-                        Venue = "O2 Arena",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[2],
-                                IsHost = true                            
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = false                            
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 7",
-                        Date = DateTime.UtcNow.AddMonths(7),
-                        Description = "Activity 7 months in future",
-                        Category = "travel",
-                        City = "Berlin",
-                        Venue = "All",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[0],
-                                IsHost = true                            
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[2],
-                                IsHost = false                            
-                            },
-                        }
-                    },
-                    new Activity
-                    {
-                        Title = "Future Activity 8",
-                        Date = DateTime.UtcNow.AddMonths(8),
-                        Description = "Activity 8 months in future",
-                        Category = "drinks",
-                        City = "London",
-                        Venue = "Pub",
-                        Attendees = new List<ActivityAttendee>
-                        {
-                            new ActivityAttendee
-                            {
-                                AppUser = users[2],
-                                IsHost = true                            
-                            },
-                            new ActivityAttendee
-                            {
-                                AppUser = users[1],
-                                IsHost = false                            
                             },
                         }
                     }
