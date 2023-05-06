ff9978e4712d4e816caaaf2b5b07e3ed2193e0ce
giou55 <giourmetakis00@gmail.com>
2023-05-06 19:53:41 +0300

    changes

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -5,7 +5,8 @@ namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context,
+        public static async Task SeedData(
+            DataContext context,
             UserManager<AppUser> userManager)
         {
             if (!userManager.Users.Any() && !context.Activities.Any())
